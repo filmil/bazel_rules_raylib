fd1116259bd1747698c8f823a34e2cc7d490d19b
Filip Filmar <filmil@gmail.com>
2025-10-30 02:28:13 -0700

    fix: install prereqs on ubuntu

---
+++ third_party/deps/ubuntu.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# Installs dependencies for running raylib on ubuntu
+sudo apt-get install -y libasound2-dev libx11-dev libxrandr-dev \
+    libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev \
+    libxinerama-dev libwayland-dev libxkbcommon-dev
+
